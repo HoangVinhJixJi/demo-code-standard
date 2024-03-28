c8bec8915388672fc9aa7e8dc4f2b80d4a7f77fa
HoangVinhJixJi <nguyenhoangvv284@gmail.com>
2024-03-28 14:28:18 +0700

    test husky

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
-    env: {
-        commonjs: true,
-        es2021: true,
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard', 'prettier'],
+  plugins: ['prettier'],
+  overrides: [
+    {
+      env: {
         node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    extends: ['standard', 'prettier'],
-    plugins: ['prettier'],
-    overrides: [
-        {
-            env: {
-                node: true,
-            },
-            files: ['.eslintrc.{js,cjs}'],
-            parserOptions: {
-                sourceType: 'script',
-            },
-        },
-    ],
-    parserOptions: {
-        ecmaVersion: 'latest',
-    },
-    rules: {
-        'prettier/prettier': 'error',
-    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
 };
